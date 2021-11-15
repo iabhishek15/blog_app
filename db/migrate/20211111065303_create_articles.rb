3939a5b58aa35084b3c2cb2f5e71d729242df717
iabhishek15 <158singhabhi@gmail.com>
2021-11-15 11:12:57 +0530

    changes made in model

--- db/migrate/20211111065303_create_articles.rb
+++ db/migrate/20211111065303_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.text :content
-      t.belongs_to :user, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20211113090925_create_comments.rb
+++ db/migrate/20211113090925_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.text :content
-      t.belongs_to :article, foreign_key: true
-      t.belongs_to :user, foreign_key: true
+      t.references :article, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
