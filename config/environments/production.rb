ea18b5e613d47d7dc810a8cf279edf316604eee9
iabhishek15 <158singhabhi@gmail.com>
2021-11-15 08:50:51 +0530

    deployment changes done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,17 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+
+  # config/environments/production.rb
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
